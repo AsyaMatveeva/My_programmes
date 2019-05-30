e30f9fac642dfb61415da8ccf901141c46934b51
Asya Matveeva <48746980+AsyaMatveeva@users.noreply.github.com>
2019-05-30 22:48:12 +0300

    Ball on the steps

---
+++ Steps.py
@@ -0,0 +1,23 @@
+n = int(input())
+c = 'way'
+def ball(n):
+    global c
+    z = 0
+    y = 0
+    x = 1
+    a = 1
+    if n == 1:
+        return(1)
+    else:
+        for i in range(2, n + 1):
+            z = y
+            #print("z =", z)
+            y = x
+            #print("y =", y)
+            x = a
+            #print("x =", x)
+            a = x + y + z
+            print("a =", a)
+        c = 'ways'
+        return(a)
+print(ball(n), c)
