630efdd51cc84a0851ff22e1f9056a53051fb073
Asya Matveeva <48746980+AsyaMatveeva@users.noreply.github.com>
2019-06-19 15:43:17 +0300

    Catalan numbers formula.
    Formula to count the given number of Catalan sequence. Catalan numbers - quantity of right arrangement of n pairs of brackets for n from 0 to infinity (C0 = 1, C1 = 1). Formula: Cn = binomial coefficient (2n, n) // (n + 1)
    Recurrent solution:

--- Catalan numbers 2.py
+++ Catalan numbers 2.py
@@ -1,8 +1,8 @@
 import math
-n = int(input())
+n = int(input()) #Number of Catalan sequence's element to count
 if n > 1:
     def C(a, b):
-        f = math.factorial(a) // (math.factorial(b) * math.factorial(a - b))
+        f = math.factorial(a) // (math.factorial(b) * math.factorial(a - b)) #Binomial coefficient(a, b)
         return(f)
     def Cat(n):
         result = C(2 * n, n) // (n + 1)
