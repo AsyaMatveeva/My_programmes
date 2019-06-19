8a0da4edd83a5b1e6b462625ba6ca4082aee3c37
Asya Matveeva <48746980+AsyaMatveeva@users.noreply.github.com>
2019-06-19 15:45:08 +0300

    Catalan numbers recurrent
    Reccurent way to count the given number of Catalan sequence. Catalan numbers - quantity of right arrangement of n pairs of brackets for n from 0 to infinity (C0 = 1, C1 = 1).

--- Catalan numbers.py
+++ Catalan numbers.py
@@ -1,11 +1,11 @@
-n = int(input()) 
-def Cat(n):
+n = int(input()) #Number of Catalan sequence's element to count
+def C_num(n):
     Sum = 0
     if n == 0 or n == 1:
         Sum = 1
     else:
         for i in range(0, n):
-            Sum += Cat(i) * Cat(n - i - 1)
+            Sum += C_num(i) * C_num(n - i - 1)
     return(Sum)
-Cat(n)
-print(Cat(n))
+C_num(n)
+print(C_num(n))
