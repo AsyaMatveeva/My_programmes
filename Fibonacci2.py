c446e5038194346bf6c85b26d099bae271039a68
Asya Matveeva <48746980+AsyaMatveeva@users.noreply.github.com>
2019-06-19 15:08:12 +0300

    Fibonacci numbers
    Input: n
    This program calculates element of Fibonacci sequence with index n - 1.

--- Fibonacci2.py
+++ Fibonacci2.py
@@ -1,14 +1,14 @@
-n = int(input())
+n = int(input()) "Number of the element in Fibonacci sequence
 def f(n):
-    x = 1
-    y = 1
+    x = 1 #previous Fibonacci number
+    y = 1 #previous for x Fibonacci number
     if n == 1:
         return(1)
     elif n == 2:
         return(1)
     else:
         for i in range(3, n + 1):
-            a = x + y
+            a = x + y "Element of Fibonacci sequence with number "i"
             x = y
             print("x =", x)
             y = a
